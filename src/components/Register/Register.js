5e5f007b3811d5efe1861e98f14b539cf0f157e6
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-12-26 00:16:40 +0100

    Basic imports and regex

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,3 +1,13 @@
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 export const Register = () => {
   return <h1>LOGOWANIE</h1>;
 };
