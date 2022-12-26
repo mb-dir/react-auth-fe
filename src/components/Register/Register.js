fa10f9ac642882e3491645e466501b07a31c06a9
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-12-26 22:43:39 +0100

    Username input

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,56 @@
+import clsx from "clsx";
 import {
   faCheck,
   faTimes,
   faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+import { useState, useRef, useEffect } from "react";
 
 export const Register = () => {
-  return <h1>LOGOWANIE</h1>;
+  const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+  const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+  const [ username, setUsername ] = useState("");
+  const [ isValidUsername, setIsValidUsername ] = useState(false);
+
+  const usernameRef = useRef(null);
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  useEffect(
+    () => {
+      setIsValidUsername(USER_REGEX.test(username));
+    },
+    [ USER_REGEX, username ]
+  );
+
+  return (
+    <section>
+      <form>
+        <label htmlFor="username">
+          Username
+          <FontAwesomeIcon
+            icon={faCheck}
+            className={clsx(isValidUsername ? "valid" : "hide")}
+          />
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={clsx(isValidUsername || !username ? "hide" : "invalid")}
+          />
+        </label>
+        <input
+          id="username"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+          required
+          autoComplete="off"
+          ref={usernameRef}
+          type="text"
+        />
+      </form>
+    </section>
+  );
 };
