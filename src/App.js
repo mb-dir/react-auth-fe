725bd541313f142d4bb179fb6335e5c3dcc869fa
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-01-11 21:47:47 +0100

    Login form view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Register } from "./components/Register";
+import { Login } from "./components/Login";
 export const App = () => {
   return (
     <main className="App">
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </main>
   );
 };
---
+++ src/components/Login/Login.js
@@ -0,0 +1,41 @@
+import { useRef, useEffect, useState } from "react";
+export const Login = () => {
+  const usernameRef = useRef(null);
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, []);
+
+  const [ username, setUsername ] = useState("");
+  const [ pwd, setPwd ] = useState("");
+
+  return (
+    <section>
+      <h1>Sign in</h1>
+
+      <form>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          ref={usernameRef}
+          onChange={({ target }) => setUsername(target.value)}
+          value={username}
+          required
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={({ target }) => setPwd(target.value)}
+          value={pwd}
+          required
+        />
+        <button>Sign in</button>
+        <p>Need an account?</p>
+        <a href="#">Click here</a>
+      </form>
+    </section>
+  );
+};
