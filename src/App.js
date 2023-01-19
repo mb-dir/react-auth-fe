ef07eaef9d781ebc7d9e6467ed333600fc73534b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-01-19 23:15:05 +0100

    Routes without auth protection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import { Register } from "./components/Register";
+import { Admin } from "./components/Admin";
+import { Editor } from "./components/Editor";
+import { Layout } from "./components/Layout";
+import { Lounge } from "./components/Lounge";
+import { Missing } from "./components/Missing";
+import { Unauthorized } from "./components/Unauthorized";
+import { LinkPage } from "./components/LinkPage";
 import { Login } from "./components/Login";
+import { Routes, Route } from "react-router-dom";
 export const App = () => {
   return (
-    <main className="App">
-      {/* <Register /> */}
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* Protected routes */}
+        <Route path="editor" element={<Editor />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="lounge" element={<Lounge />} />
+
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 };
