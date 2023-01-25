ab1a8839d0eb5913657ba16f5a29e9229421deb4
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-01-25 23:15:12 +0100

    Handling an expired refresh token

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
-import axios from "../../api/axios";
+import { useAxiosPrivate  } from "../../hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export const Users = () => {
   const [ users, setUsers ] = useState();
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     let isMounted = true;
@@ -10,7 +14,7 @@ export const Users = () => {
 
     const getUsers = async () => {
       try {
-        const res = await axios.get("/users", {
+        const res = await axiosPrivate.get("/users", {
           signal: controller.signal,
         });
 
@@ -19,6 +23,7 @@ export const Users = () => {
         isMounted && setUsers(res.data);
       } catch (error) {
         console.log(error);
+        navigate('/login', {state: {from: location}, replace: true})
       }
     };
     getUsers();
@@ -33,7 +38,7 @@ export const Users = () => {
     <article>
       <h2>Users list</h2>
       {users?.length ? (
-        <ul>{(users || []).map((user, i) => <li key={i}>{user.name}</li>)}</ul>
+        <ul>{(users || []).map((user, i) => <li key={i}>{user.username}</li>)}</ul>
       ) : (
         <p>No users</p>
       )}
