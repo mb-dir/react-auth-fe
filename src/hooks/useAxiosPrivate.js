934ae17f90013cc3770c693d46e736bcae327441
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-01-26 15:08:26 +0100

    Fix problem with hook exports

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useRefreshToken } from "./useRefreshToken";
 import { useAuth } from "./useAuth";
 
-export const useAxiosPrivate = () => {
+const useAxiosPrivate = () => {
   const refresh = useRefreshToken();
   const { auth } = useAuth();
 
@@ -42,3 +42,5 @@ export const useAxiosPrivate = () => {
 
   return axiosPrivate;
 };
+
+export default useAxiosPrivate;
