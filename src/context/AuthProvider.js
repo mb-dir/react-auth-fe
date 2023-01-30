5105c346990f4d209b43ffdfe749985d70142d51
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-01-30 23:49:09 +0100

    Fix localStorage logic related with persist

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -4,11 +4,11 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [ auth, setAuth ] = useState({});
-  const [ isPersist, setIsPersist ] = useState(
-    JSON.parse(localStorage.getItem("persist")) || false
-  );
+  const [ isPersist, setIsPersist ] = useState(() => {
+    return JSON.parse(localStorage.getItem("persist") || false);
+  });
   return (
-    <AuthContext.Provider value={{ auth, setAuth, isPersist }}>
+    <AuthContext.Provider value={{ auth, setAuth, isPersist, setIsPersist }}>
       {children}
     </AuthContext.Provider>
   );
