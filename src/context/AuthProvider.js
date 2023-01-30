adadad0f9775c43e2041895bce74ae5a4be2913a
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-01-30 23:37:29 +0100

    Add persist value to auth context

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -4,8 +4,11 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [ auth, setAuth ] = useState({});
+  const [ isPersist, setIsPersist ] = useState(
+    JSON.parse(localStorage.getItem("persist")) || false
+  );
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, isPersist }}>
       {children}
     </AuthContext.Provider>
   );
