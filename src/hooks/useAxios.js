667cce049e3967a09db46aa7d1ebe8545929191b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-02-12 22:53:28 +0100

    Refacotr useAxios hook

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,10 +1,16 @@
 import axios from "../api/axios";
 
 const useAxios = () => {
-  const fetchData = async (method, url, options = null, body = null) => {
+  const fetchData = async (url, options = null) => {
     try {
-      console.log(body);
-      const { data } = await axios[method](url, body, options);
+      console.log(url, options);
+      const { data } = await axios({
+        method: options.method || "get",
+        url,
+        withCredentials: options.withCredentials || false,
+        data: options.payload,
+        headers: options.headers,
+      });
       return data;
     } catch (error) {
       console.error(error);
