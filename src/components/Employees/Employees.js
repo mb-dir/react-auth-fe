ed02ae3f0f72f7912ca3417e81f5cd7ef637354a
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-02-14 01:58:31 +0100

    Add possibility to delete an employee

--- src/components/Employees/Employees.js
+++ src/components/Employees/Employees.js
@@ -1,28 +1,37 @@
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const Employees = () => {
   const fetchData = useAxiosPrivate();
   const [ employees, setEmployees ] = useState([]);
 
-  useEffect(() => {
-    const getEmployees = async () => {
+  const getEmployees = useCallback(async () => {
       try {
         const res = await fetchData("/employees");
-        const employeesnames = res.map(employee=>`${employee.firstname} ${employee.lastname}`);
-        setEmployees(employeesnames);
+        setEmployees(res);
       } catch (error) {
         console.error(error);
       }
-    };
-    getEmployees();
+    },[fetchData]);
+
+  useEffect(() => {
+      getEmployees();
   }, []);
+
+  const deleteEmployee = async(id)=>{
+    try {
+      await fetchData("/employees", {method: 'delete', payload: {id}});
+      getEmployees();
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <article>
       <h2>Employees list</h2>
       {employees?.length ? (
-        <ul>{(employees || []).map((user, i) => <li key={i}>{user}</li>)}</ul>
+        <ul>{(employees || []).map((employee) => <li key={employee._id}>{`${employee.firstname} ${employee.lastname}`} <button className="smallBtn" onClick={()=>deleteEmployee(employee._id)}>X</button></li>)}</ul>
       ) : (
         <p>No employees</p>
       )}
